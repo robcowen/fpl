3ed212000def88d605accf4055d4cde42f40efea
Rob <robcowen@gmail.com>
2023-11-05 12:19:41 +0000

    Revert "First commit"
    This reverts commit 3245e1563c1fc1264568160269b8db3d0a376807.

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-# class LeagueInputForm(FlaskForm):
-#     league_id = StringField('League ID', validators=[DataRequired()], description="Enter your league id")
-#     submit = SubmitField('Generate')
+class LeagueInputForm(FlaskForm):
+    league_id = StringField('League ID', validators=[DataRequired()], description="Enter your league id")
+    submit = SubmitField('Generate')
